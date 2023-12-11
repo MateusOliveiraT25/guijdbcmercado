0f3cbae93f10977ea4cea32bb7bdfee49b6e0e22
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-12-11 19:06:53 -0300

    11/12

--- guijdbcmercado/src/main/java/View/VendasPainel.java
+++ guijdbcmercado/src/main/java/View/VendasPainel.java
@@ -2,12 +2,13 @@ package View;
 
 import Controller.EstoqueControll;
 import Model.Produto;
-import java.util.List;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VendasPainel extends JPanel {
 
@@ -16,102 +17,128 @@ public class VendasPainel extends JPanel {
     private JList<String> produtosList;
     private JCheckBox clienteVipCheckBox;
     private JButton adicionarProdutoButton, removerProdutoButton;
-    private EstoqueControll estoqueControll; // Adiciona um campo para o EstoqueControll
+    private EstoqueControll estoqueControll;
+    private JButton avancarButton;
+    private JLabel totalLabel; // Adiciona um campo para exibir o total
+    private double total; // Adiciona um campo para armazenar o total dos produtos
 
-   private JButton avancarButton; // Adiciona um campo para o botão "Avançar"
+    public VendasPainel(EstoqueControll estoqueControll) {
+        this.estoqueControll = estoqueControll;
+        setLayout(new BorderLayout());
 
-public VendasPainel(EstoqueControll estoqueControll) {
-    this.estoqueControll = estoqueControll;
-    setLayout(new BorderLayout());
+        codigoBarrasField = new JTextField();
+        adicionarProdutoButton = new JButton("Adicionar Produto");
 
-    codigoBarrasField = new JTextField();
-    adicionarProdutoButton = new JButton("Adicionar Produto");
+        produtosListModel = new DefaultListModel<>();
+        produtosList = new JList<>(produtosListModel);
+        JScrollPane produtosScrollPane = new JScrollPane(produtosList);
 
-    produtosListModel = new DefaultListModel<>();
-    produtosList = new JList<>(produtosListModel);
-    JScrollPane produtosScrollPane = new JScrollPane(produtosList);
+        removerProdutoButton = new JButton("Remover Produto");
+        clienteVipCheckBox = new JCheckBox("Cliente VIP");
 
-    removerProdutoButton = new JButton("Remover Produto");
-    clienteVipCheckBox = new JCheckBox("Cliente VIP");
+        avancarButton = new JButton("Pagamento");
+        totalLabel = new JLabel("Total: R$ 0.00"); // Inicializa o rótulo de total
 
-    avancarButton = new JButton("Pagamento"); // Inicializa o botão "Avançar"
+        JPanel codigoBarrasPanel = new JPanel(new BorderLayout());
+        codigoBarrasPanel.add(new JLabel("Código de Barras: "), BorderLayout.WEST);
+        codigoBarrasPanel.add(codigoBarrasField, BorderLayout.CENTER);
+        codigoBarrasPanel.add(adicionarProdutoButton, BorderLayout.EAST);
 
-    JPanel codigoBarrasPanel = new JPanel(new BorderLayout());
-    codigoBarrasPanel.add(new JLabel("Código de Barras: "), BorderLayout.WEST);
-    codigoBarrasPanel.add(codigoBarrasField, BorderLayout.CENTER);
-    codigoBarrasPanel.add(adicionarProdutoButton, BorderLayout.EAST);
+        JPanel botoesPanel = new JPanel();
+        botoesPanel.add(removerProdutoButton);
+        botoesPanel.add(clienteVipCheckBox);
+        botoesPanel.add(avancarButton);
+        botoesPanel.add(totalLabel); // Adiciona o rótulo de total aos botõesPanel
 
-    JPanel botoesPanel = new JPanel();
-    botoesPanel.add(removerProdutoButton);
-    botoesPanel.add(clienteVipCheckBox);
-    botoesPanel.add(avancarButton); // Adiciona o botão "Avançar"
+        add(codigoBarrasPanel, BorderLayout.NORTH);
+        add(produtosScrollPane, BorderLayout.CENTER);
+        add(botoesPanel, BorderLayout.SOUTH);
 
-    add(codigoBarrasPanel, BorderLayout.NORTH);
-    add(produtosScrollPane, BorderLayout.CENTER);
-    add(botoesPanel, BorderLayout.SOUTH);
+        adicionarProdutoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                adicionarProduto();
+            }
+        });
 
-    adicionarProdutoButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            adicionarProduto();
-        }
-    });
+        removerProdutoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removerProduto();
+            }
+        });
 
-    removerProdutoButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            removerProduto();
-        }
-    });
-    avancarButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Lógica para avançar para a tela de conclusão
-            JTabbedPane jTPane = (JTabbedPane) SwingUtilities.getAncestorOfClass(JTabbedPane.class, VendasPainel.this);
-    
-            // Supondo que "Tela de Conclusão de Compras" está na terceira posição (índice 2)
-            jTPane.setSelectedIndex(2);
-    
-            // Obtém a referência para o ConclusaoCompraPainel
-            ConclusaoCompraPainel conclusaoCompraPainel = (ConclusaoCompraPainel) jTPane.getComponentAt(2);
-    
-            // Obtém os produtos do VendasPainel
-            List<String> produtos = obterProdutosDoVendasPainel();
-    
-            // Passa os produtos para o ConclusaoCompraPainel
-            conclusaoCompraPainel.setProdutos(produtos);
-        }
-    
-        private List<String> obterProdutosDoVendasPainel() {
-            // Lógica para obter a lista de produtos do VendasPainel
-            List<String> produtos = new ArrayList<>();
-            // Adicione aqui a lógica para obter os produtos do VendasPainel e formatá-los como desejado
-            return produtos;
-        }
-    });
-    
+        avancarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Lógica para avançar para a tela de conclusão
+                JTabbedPane jTPane = (JTabbedPane) SwingUtilities.getAncestorOfClass(JTabbedPane.class, VendasPainel.this);
 
-    
-}
+                // Supondo que "Tela de Conclusão de Compras" está na terceira posição (índice 2)
+                jTPane.setSelectedIndex(2);
 
-private void adicionarProduto() {
-    String codigoBarras = codigoBarrasField.getText();
-    if (!codigoBarras.isEmpty()) {
-        Produto produto = estoqueControll.obterProdutoPorCodigoBarras(codigoBarras);
+                // Obtém a referência para o ConclusaoCompraPainel
+                ConclusaoCompraPainel conclusaoCompraPainel = (ConclusaoCompraPainel) jTPane.getComponentAt(2);
 
-        if (produto != null) {
-            produtosListModel.addElement(produto.getNome() + " - Preço: R$" + produto.getPreco());
-            codigoBarrasField.setText("");
-        } else {
-            JOptionPane.showMessageDialog(this, "Produto não encontrado no estoque", "Erro", JOptionPane.ERROR_MESSAGE);
+                // Obtém os produtos do VendasPainel
+                List<String> produtos = obterProdutosDoVendasPainel();
+
+                // Passa os produtos para o ConclusaoCompraPainel
+                conclusaoCompraPainel.setProdutos(produtos);
+            }
+
+            private List<String> obterProdutosDoVendasPainel() {
+                // Lógica para obter a lista de produtos do VendasPainel
+                List<String> produtos = new ArrayList<>();
+                // Adicione aqui a lógica para obter os produtos do VendasPainel e formatá-los como desejado
+                return produtos;
+            }
+        });
+    }
+
+    private void adicionarProduto() {
+        String codigoBarras = codigoBarrasField.getText();
+        if (!codigoBarras.isEmpty()) {
+            Produto produto = estoqueControll.obterProdutoPorCodigoBarras(codigoBarras);
+
+            if (produto != null) {
+                produtosListModel.addElement(produto.getNome() + " - Preço: R$" + produto.getPreco());
+                codigoBarrasField.setText("");
+
+                // Atualiza o total ao adicionar um produto
+                total += produto.getPreco();
+                atualizarTotalLabel();
+            } else {
+                JOptionPane.showMessageDialog(this, "Produto não encontrado no estoque", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
-}
 
-private void removerProduto() {
-    int selectedIndex = produtosList.getSelectedIndex();
-    if (selectedIndex != -1) {
-        produtosListModel.remove(selectedIndex);
+    private void removerProduto() {
+        int selectedIndex = produtosList.getSelectedIndex();
+        if (selectedIndex != -1) {
+            // Obtém o texto do item selecionado para extrair o preço
+            String selectedProduct = produtosListModel.getElementAt(selectedIndex);
+            double preco = extrairPrecoDoTexto(selectedProduct);
+
+            // Atualiza o total ao remover um produto
+            total -= preco;
+            atualizarTotalLabel();
+
+            // Remove o produto da lista
+            produtosListModel.remove(selectedIndex);
+        }
     }
-}
+
+    private double extrairPrecoDoTexto(String textoProduto) {
+        // Lógica para extrair o preço do texto formatado
+        // Implemente de acordo com o formato real do texto
+        // Neste exemplo, assume-se que o preço está no final do texto após "Preço: R$"
+        String precoTexto = textoProduto.substring(textoProduto.lastIndexOf("R$") + 3).trim();
+        return Double.parseDouble(precoTexto);
+    }
+
+    private void atualizarTotalLabel() {
+        totalLabel.setText("Total: R$" + String.format("%.2f", total));
+    }
 }
