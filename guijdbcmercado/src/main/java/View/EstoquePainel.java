5e1db750dd7a2bfd992aeafdeec6fa15759d8371
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-12-11 21:23:02 -0300

    11/12

--- guijdbcmercado/src/main/java/View/EstoquePainel.java
+++ guijdbcmercado/src/main/java/View/EstoquePainel.java
@@ -8,8 +8,7 @@ import javax.swing.*;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EstoquePainel extends JPanel {
@@ -32,8 +31,19 @@ public class EstoquePainel extends JPanel {
             }
         });
 
+        JButton removerProdutoButton = new JButton("Remover Produto");
+        removerProdutoButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                removerProduto();
+            }
+        });
+
+        
+
         add(listarProdutosButton);
         add(adicionarProdutoButton);
+        add(removerProdutoButton);
+        
     }
 
     public void setGerenciadorEstoque(EstoqueControll gerenciadorEstoque) {
@@ -52,83 +62,89 @@ public class EstoquePainel extends JPanel {
     }
 
     private void adicionarProduto() {
-    // Criação do JDialog
-    JDialog dialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(this), "Adicionar Produto", true);
-    dialog.setLayout(new GridLayout(5, 2));
+        // Criação do JDialog
+        JDialog dialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(this), "Adicionar Produto", true);
+        dialog.setLayout(new GridLayout(5, 2));
 
-    // Adiciona campos de entrada e rótulos ao JDialog
-    JTextField codigoBarraField = new JTextField();
-    JTextField nomeField = new JTextField();
-    JTextField quantidadeField = new JTextField();
-    JTextField precoField = new JTextField();
+        // Adiciona campos de entrada e rótulos ao JDialog
+        JTextField codigoBarraField = new JTextField();
+        JTextField nomeField = new JTextField();
+        JTextField quantidadeField = new JTextField();
+        JTextField precoField = new JTextField();
 
-    dialog.add(new JLabel("Código de Barras:"));
-    dialog.add(codigoBarraField);
-    dialog.add(new JLabel("Nome:"));
-    dialog.add(nomeField);
-    dialog.add(new JLabel("Quantidade:"));
-    dialog.add(quantidadeField);
-    dialog.add(new JLabel("Preço:"));
-    dialog.add(precoField);
+        dialog.add(new JLabel("Código de Barras:"));
+        dialog.add(codigoBarraField);
+        dialog.add(new JLabel("Nome:"));
+        dialog.add(nomeField);
+        dialog.add(new JLabel("Quantidade:"));
+        dialog.add(quantidadeField);
+        dialog.add(new JLabel("Preço:"));
+        dialog.add(precoField);
 
-    // Adiciona botões de OK e Cancelar ao JDialog
-    JButton okButton = new JButton("OK");
-    JButton cancelButton = new JButton("Cancelar");
+        // Adiciona botões de OK e Cancelar ao JDialog
+        JButton okButton = new JButton("OK");
+        JButton cancelButton = new JButton("Cancelar");
 
-    dialog.add(okButton);
-    dialog.add(cancelButton);
+        dialog.add(okButton);
+        dialog.add(cancelButton);
 
-    // Adiciona ação ao botão OK
-    // Adiciona ação ao botão OK
-    okButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            try {
-                // Obtem os valores dos campos
-                String codigoBarra = codigoBarraField.getText();
-                String nome = nomeField.getText();
-                int quantidade = Integer.parseInt(quantidadeField.getText());
-                double preco = Double.parseDouble(precoField.getText());
+        // Adiciona ação ao botão OK
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Obtem os valores dos campos
+                    String codigoBarra = codigoBarraField.getText();
+                    String nome = nomeField.getText();
+                    int quantidade = Integer.parseInt(quantidadeField.getText());
+                    double preco = Double.parseDouble(precoField.getText());
 
-                // Verifica se o código de barras já existe
-                if (gerenciadorEstoque.codigoDeBarrasExiste(codigoBarra)) {
-                    JOptionPane.showMessageDialog(dialog, "Código de barras já existe. Por favor, escolha outro.", "Erro",
+                    // Chamada do método do controlador para adicionar o produto
+                    gerenciadorEstoque.adicionarProduto(codigoBarra, nome, quantidade, preco);
+
+                    // Exibição de mensagem de sucesso em uma caixa de diálogo
+                    JOptionPane.showMessageDialog(EstoquePainel.this, "Produto adicionado com sucesso!", "Sucesso",
+                            JOptionPane.INFORMATION_MESSAGE);
+
+                    // Fecha o JDialog
+                    dialog.dispose();
+                } catch (NumberFormatException ex) {
+                    // Tratamento de erro se a conversão falhar
+                    JOptionPane.showMessageDialog(dialog, "Erro ao converter valores para números.", "Erro",
                             JOptionPane.ERROR_MESSAGE);
-                    return;  // Aborta a operação se o código de barras já existir
                 }
+            }
+        });
 
-                // Chamada do método do controlador para adicionar o produto
-                gerenciadorEstoque.adicionarProduto(codigoBarra, nome, quantidade, preco);
-
-                // Exibição de mensagem de sucesso em uma caixa de diálogo
-                JOptionPane.showMessageDialog(EstoquePainel.this, "Produto adicionado com sucesso!", "Sucesso",
-                        JOptionPane.INFORMATION_MESSAGE);
-
-                // Fecha o JDialog
+        // Adiciona ação ao botão Cancelar
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Fecha o JDialog sem fazer alterações
                 dialog.dispose();
-            } catch (NumberFormatException ex) {
-                // Tratamento de erro se a conversão falhar
-                JOptionPane.showMessageDialog(dialog, "Erro ao converter valores para números.", "Erro",
-                        JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        // Configurações finais do JDialog
+        dialog.setSize(300, 200);
+        dialog.setLocationRelativeTo(null); // Centraliza na tela
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        // Exibe o JDialog
+        dialog.setVisible(true);
+    }
+
+    private void removerProduto() {
+        String codigoBarra = JOptionPane.showInputDialog(this, "Digite o código de barras do produto a ser removido:");
+        if (codigoBarra != null && !codigoBarra.isEmpty()) {
+            try {
+                gerenciadorEstoque.removerProduto(codigoBarra);
+                JOptionPane.showMessageDialog(this, "Produto removido com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(this, "Erro ao remover produto: " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         }
-    });
+    }
 
-    // Adiciona ação ao botão Cancelar
-    cancelButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Fecha o JDialog sem fazer alterações
-            dialog.dispose();
-        }
-    });
-
-    // Configurações finais do JDialog
-    dialog.setSize(300, 200);
-    dialog.setLocationRelativeTo(null); // Centraliza na tela
-    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-
-    // Exibe o JDialog
-    dialog.setVisible(true);
-}
+  
 }
